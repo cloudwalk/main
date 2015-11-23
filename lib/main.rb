2d464b7e895dadca590ec2f7ac49bf19f3ceffb1
Thiago Scalone <thiago@scalone.com.br>
2015-11-23 16:00:01 -0200

    Fix Time display.

--- lib/main.rb
+++ lib/main.rb
@@ -9,7 +9,7 @@ class Main < Device
     Device.app_loop do
       time = Time.now
       Device::Display.print_bitmap("./shared/walk.bmp",0,0)
-      Device::Display.print(" #{time.day}/#{time.month}/#{time.year}   #{time.hour}:#{rjust(time.min, 2, "0")}", 6, 0)
+      Device::Display.print("#{time.day}/#{time.month}/#{time.year} #{time.hour}:#{rjust(time.min, 2, "0")}", 12, 0)
       puts ""
       case getc(2000)
       when Device::IO::ENTER
