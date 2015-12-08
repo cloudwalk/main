b14db3c80f0b8db5a07e3053caff37c71851df99
Thiago Scalone <thiago@scalone.com.br>
2015-12-08 16:17:20 -0200

    Remove unnecessary display on application loop, it was need on the past to fix a bug on Screen(ruby-pax).

--- lib/main.rb
+++ lib/main.rb
@@ -9,7 +9,6 @@ class Main < Device
     Device.app_loop do
       time = Time.now
       Device::Display.print_bitmap("./shared/walk.bmp",0,0)
-      puts ""
       Device::Display.print("#{rjust(time.day.to_s, 2, "0")}/#{rjust(time.month.to_s, 2, "0")}/#{time.year} #{rjust(time.hour.to_s, 2, "0")}:#{rjust(time.min.to_s, 2, "0")}", 6, 0)
       case getc(2000)
       when Device::IO::ENTER
