1259e00e4e88026e44955ddbf8df2be37ded25dd
Thiago Scalone <thiago@scalone.com.br>
2015-12-08 16:16:25 -0200

    Refactoring datetime display to rjust with “0”.

--- lib/main.rb
+++ lib/main.rb
@@ -9,8 +9,8 @@ class Main < Device
     Device.app_loop do
       time = Time.now
       Device::Display.print_bitmap("./shared/walk.bmp",0,0)
-      Device::Display.print("#{time.day}/#{time.month}/#{time.year} #{time.hour}:#{rjust(time.min, 2, "0")}", 12, 0)
       puts ""
+      Device::Display.print("#{rjust(time.day.to_s, 2, "0")}/#{rjust(time.month.to_s, 2, "0")}/#{time.year} #{rjust(time.hour.to_s, 2, "0")}:#{rjust(time.min.to_s, 2, "0")}", 6, 0)
       case getc(2000)
       when Device::IO::ENTER
         Cloudwalk.start
@@ -26,3 +26,4 @@ class Main < Device
     "0.1.0"
   end
 end
+
