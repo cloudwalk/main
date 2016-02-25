510676b94880823af62bfbac51075fbd1c12b36c
Thiago Scalone <thiago@scalone.com.br>
2016-02-25 20:36:49 -0300

    Move walk.bmp to main.bmp and use Dafunk to print bmp image.

--- lib/main.rb
+++ lib/main.rb
@@ -7,7 +7,7 @@ class Main < Device
     Device::Display.clear
 
     Device.app_loop do
-      Device::Display.print_bitmap("./shared/walk.bmp",0,0)
+      Device::Display.print_main_image
       Device::Display.print(I18n.t(:time, :time => Time.now), 6, 0)
       case getc(2000)
       when Device::IO::ENTER
