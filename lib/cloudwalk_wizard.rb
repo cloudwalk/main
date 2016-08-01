4125727ea863bb8ba2166c09a9215f96737d3453
Thiago Scalone <thiago@scalone.com.br>
2016-08-01 10:37:57 -0300

    Refactoring CloudwalkWizard adapting new Screenflow syntax.

--- lib/cloudwalk_wizard.rb
+++ lib/cloudwalk_wizard.rb
@@ -1,13 +1,9 @@
 class CloudwalkWizard < DaFunk::ScreenFlow
   include DaFunk::Helper
 
-  def order
-    language.serial_number_1.serial_number_2.logical_number_1.
-      logical_number_2.communication_1.communication_2.
-      activation_1.activation_2.activation_3
-  end
+  setup do Device::Display.clear end
 
-  add :language do |result|
+  screen :language do |result|
     I18n.pt :language
     # TODO Language
     if (try_key(["1","2"], 0) == "1")
@@ -19,19 +15,19 @@ class CloudwalkWizard < DaFunk::ScreenFlow
     true
   end
 
-  add :serial_number_1 do |result|
+  screen :serial_number_1 do |result|
     confirm I18n.t(:serial_number_1)
   end
 
-  add :serial_number_2 do |result|
+  screen :serial_number_2 do |result|
     confirm I18n.t(:serial_number_2, :args => Device::System.serial)
   end
 
-  add :logical_number_1 do |result|
+  screen :logical_number_1 do |result|
     confirm I18n.t(:logical_number_1)
   end
 
-  add :logical_number_2 do |result|
+  screen :logical_number_2 do |result|
     I18n.pt(:logical_number_2)
     options = {:value => Device::Setting.logical_number, :mode => :alpha,
       :column => 0, :line => 4, :label => ": "}
@@ -39,11 +35,11 @@ class CloudwalkWizard < DaFunk::ScreenFlow
     ! Device::Setting.logical_number.empty?
   end
 
-  add :communication_1 do |result|
+  screen :communication_1 do |result|
     confirm I18n.t(:communication)
   end
 
-  add :communication_2 do |result|
+  screen :communication_2 do |result|
     ret = false
     unless (ret = (Device::Network.connected? == 0))
       case(menu("Select Media:", {"WIFI" => :wifi, "GPRS" => :gprs}))
@@ -56,11 +52,11 @@ class CloudwalkWizard < DaFunk::ScreenFlow
     ret
   end
 
-  add :activation_1 do |result|
+  screen :activation_1 do |result|
     confirm I18n.t(:activation_1)
   end
 
-  add :activation_2 do |result|
+  screen :activation_2 do |result|
     I18n.pt(:activation_2)
     connected = Device::Network.connected?
     connected = Device::Network.attach if connected != Device::Network::SUCCESS
@@ -74,7 +70,7 @@ class CloudwalkWizard < DaFunk::ScreenFlow
     end
   end
 
-  add :activation_3 do |result|
+  screen :activation_3 do |result|
     confirm I18n.t(:activation_success)
   end
 end
