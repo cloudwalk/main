7bc21da13a61938de0cdd347a2bd8ace4ebb5f1d
Thiago Scalone <thiago@scalone.com.br>
2017-12-01 12:26:14 -0200

    Support to display merchant name

---
+++ lib/merchant_name.rb
@@ -0,0 +1,21 @@
+class MerchantName
+  def self.display
+    if (merchant_name = "#{Device::ParamsDat.file["merchant_name"]}") != ""
+      Device::Display.print_line(merchant_name, self.line, self.align(merchant_name))
+    end
+  end
+
+  def self.line
+    case Device::System.model.to_s.downcase
+    when "gpos400"
+      16
+    else
+      STDOUT.max_y - 2
+    end
+  end
+
+  def self.align(merchant_name)
+    ((STDOUT.max_x - merchant_name.size) / 2).to_i
+  end
+end
+
