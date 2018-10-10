ccb85e5e563af2d5809eb948a010faa05623499f
William Roberto <william.roberto.t@gmail.com>
2018-10-10 18:14:06 -0300

    Added new class InjectedKeys.

---
+++ lib/injected_keys.rb
@@ -0,0 +1,14 @@
+class InjectedKeys
+  def self.write_injected_keys_on_log
+    self.write_keys
+  end
+
+  def self.write_keys
+    (0..99).inject({}) do |hash, slot|
+      duktp_key = Device::Pinpad.key_ksn(slot)
+      master_session = Device::Pinpad.key_kcv(slot)
+      ContextLog.info("[K] Dukpt key injetecd on slot: #{slot}, ksi: #{duktp_key[:pin][1]}", true ) if duktp_key[:pin][0] == 0
+      ContextLog.info("[K] Master session key injetecd on slot: #{slot}, kcv: #{master_session[:ms3des][1]}", true) if master_session[:ms3des][0] == 0
+    end
+  end
+end
