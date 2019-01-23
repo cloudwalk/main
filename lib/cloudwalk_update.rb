ce3a03a68966658adad55bb9c63c0440e0a5d8c3
Thiago Scalone <thiago@scalone.com.br>
2019-01-23 16:02:20 -0200

    Implement CloudwalkUpdate.system routine
    First implementation of UX system update improvement process

--- lib/cloudwalk_update.rb
+++ lib/cloudwalk_update.rb
@@ -18,4 +18,25 @@ class CloudwalkUpdate
       DaFunk::ParamsDat.update_apps(true)
     end
   end
+
+  def self.system
+    return unless Device::Network.connected?
+
+    Device::Display.clear
+    I18n.pt(:system_update_check)
+    Device::Display.print(I18n.t(:system_update_cancel), 2)
+    Device::Display.print("", 3)
+    key = Device::IO::KEY_TIMEOUT
+
+    10.times do |i|
+      Device::Display.print((10 - i).to_s, 5, 11)
+      key = getc(1000)
+      break if key != Device::IO::KEY_TIMEOUT
+    end
+
+    if key != Device::IO::CANCEL
+      SystemUpdate.new.start
+    end
+  end
 end
+
