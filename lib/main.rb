683fdf2ab691c9abe8c9bee113b5c6db7358412c
Thiago Scalone <thiago@scalone.com.br>
2019-02-16 20:04:51 -0200

    Force payment channel creation if it doesn’t exists on comm thread
    Logic is trigged if a write message (from main thread) is queued

--- lib/main.rb
+++ lib/main.rb
@@ -104,6 +104,7 @@ class Main < Device
           Context::ThreadScheduler.execute(ThreadScheduler::THREAD_COMMUNICATION)
 
           if buf = Context::ThreadChannel.queue_read(ThreadScheduler::THREAD_COMMUNICATION)
+            DaFunk::PaymentChannel.connect unless DaFunk::PaymentChannel.client
             DaFunk::PaymentChannel.client.write(buf)
           end
         end
