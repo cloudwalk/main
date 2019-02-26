6092cff6bc033986d3f96014585d25f9b30567f2
William Roberto <william.roberto.t@gmail.com>
2019-02-26 16:56:34 -0300

    Contextlog.info takes only one argument.

--- lib/injected_keys.rb
+++ lib/injected_keys.rb
@@ -7,8 +7,8 @@ class InjectedKeys
     (0..99).inject({}) do |hash, slot|
       duktp_key = Device::Pinpad.key_ksn(slot)
       master_session = Device::Pinpad.key_kcv(slot)
-      ContextLog.info("[K] Dukpt key injetecd on slot: #{slot}, ksi: #{duktp_key[:pin][1]}", true ) if duktp_key[:pin][0] == 0
-      ContextLog.info("[K] Master session key injetecd on slot: #{slot}, kcv: #{master_session[:ms3des][1]}", true) if master_session[:ms3des][0] == 0
+      ContextLog.info("[K] Dukpt key injetecd on slot: #{slot}, ksi: #{duktp_key[:pin][1]}") if duktp_key[:pin][0] == 0
+      ContextLog.info("[K] Master session key injetecd on slot: #{slot}, kcv: #{master_session[:ms3des][1]}") if master_session[:ms3des][0] == 0
     end
   end
 end
