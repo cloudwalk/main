6bac7be1832e0fc6ddd807ff1ac716f58bb46a92
William Roberto <william.roberto.t@gmail.com>
2019-02-27 15:19:49 -0300

    Better way of writing injected keys on log file.

--- lib/log_control.rb
+++ lib/log_control.rb
@@ -1,4 +1,16 @@
 class LogControl
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def write_keys_on_log
+    write_keys! unless log_ready?
+  end
+
+  def self.upload_log_file
+    LogsMenu.send_file(self.get_log_file)
+  end
+
   def self.delete_old_logs
     dirs = Dir.entries("./main").select { |p| p.include?(".log") }
     date_today = self.get_date_today
@@ -9,11 +21,23 @@ class LogControl
     end
   end
 
-  def self.write_keys_on_log
-    self.write_keys!
+  private
+
+  def write_keys!
+    InjectedKeys.write_injected_keys_on_log(@filename)
   end
 
-  private
+  def log_ready?
+    File.read(@filename).each_line do |line|
+      return true if line[0..2] == "[K]"
+    end
+    false
+  end
+
+  def self.get_log_file
+    time = Time.now
+    "#{"%d-%02d-%02d" % [time.year, time.month, time.day]}.log"
+  end
 
   def self.compare_dates(text1, text2)
     date = text1.match(/([0-9]+)-([0-9]+)-([0-9]+)/)
@@ -27,13 +51,5 @@ class LogControl
   def self.get_date_today
     time = Time.now
     "%d-%02d-%02d %02d:%02d:%02d:%06d" % [time.year, time.month, time.day, time.hour, time.min, time.sec, time.usec]
-  end
-
-  def self.write_keys!
-    time = Time.now
-    logname = "./main/#{"%d-%02d-%02d" % [time.year, time.month, time.day]}.log"
-    if !File.exists?(logname)
-      InjectedKeys.write_injected_keys_on_log
-    end
   end
 end
