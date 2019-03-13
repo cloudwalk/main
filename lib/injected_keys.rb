8806f4cff85f46c5a4e7b4833db4d106f58e0145
Thiago Scalone <thiago@scalone.com.br>
2019-03-13 12:36:58 -0300

    Bug fix injected keys log typo

--- lib/injected_keys.rb
+++ lib/injected_keys.rb
@@ -4,8 +4,8 @@ class InjectedKeys
     (0..99).inject({}) do |hash, slot|
       duktp_key = Device::Pinpad.key_ksn(slot)
       master_session = Device::Pinpad.key_kcv(slot)
-      ContextLog.info("[K] Dukpt key injetecd on slot: #{slot}, ksi: #{duktp_key[:pin][1]}", filename) if duktp_key[:pin][0] == 0
-      ContextLog.info("[K] Master session key injetecd on slot: #{slot}, kcv: #{master_session[:ms3des][1]}", filename) if master_session[:ms3des][0] == 0
+      ContextLog.info("[K] Dukpt key injetecd on slot: #{slot}, ksi: #{duktp_key[:pin][1]}") if duktp_key[:pin][0] == 0
+      ContextLog.info("[K] Master session key injetecd on slot: #{slot}, kcv: #{master_session[:ms3des][1]}") if master_session[:ms3des][0] == 0
     end
     ContextLog.file_log = nil
   end
