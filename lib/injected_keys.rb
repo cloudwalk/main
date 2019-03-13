3deffbdd57c14b8e62621d0ff6770db5af43db49
Thiago Scalone <thiago@scalone.com.br>
2019-03-12 21:15:36 -0300

    Refactoring InjectedKeys::write_injected_keys_on_log
    Adopt ContextLog::info and ::file_log function instead ::append_old_logs

--- lib/injected_keys.rb
+++ lib/injected_keys.rb
@@ -1,10 +1,12 @@
 class InjectedKeys
-  def self.write_injected_keys_on_log(filename)
+  def self.log(filename)
+    ContextLog.file_log = filename
     (0..99).inject({}) do |hash, slot|
       duktp_key = Device::Pinpad.key_ksn(slot)
       master_session = Device::Pinpad.key_kcv(slot)
-      ContextLog.append_old_log("[K] Dukpt key injetecd on slot: #{slot}, ksi: #{duktp_key[:pin][1]}", filename) if duktp_key[:pin][0] == 0
-      ContextLog.append_old_log("[K] Master session key injetecd on slot: #{slot}, kcv: #{master_session[:ms3des][1]}", filename) if master_session[:ms3des][0] == 0
+      ContextLog.info("[K] Dukpt key injetecd on slot: #{slot}, ksi: #{duktp_key[:pin][1]}", filename) if duktp_key[:pin][0] == 0
+      ContextLog.info("[K] Master session key injetecd on slot: #{slot}, kcv: #{master_session[:ms3des][1]}", filename) if master_session[:ms3des][0] == 0
     end
+    ContextLog.file_log = nil
   end
 end
