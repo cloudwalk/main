91bdf8ad71da1055818bf4119ec19f00da32862b
Thiago Scalone <thiago@scalone.com.br>
2020-05-28 02:12:54 -0300

    Remove stop of engine if memory consumption is to high

--- lib/cloudwalk_setup.rb
+++ lib/cloudwalk_setup.rb
@@ -448,12 +448,6 @@ class CloudwalkSetup
       LogControl.upload
     end
 
-    DaFunk::EventHandler.new :schedule, minutes: 10 do
-      if Vm.current_memory > 14_000_000
-        DaFunk::Engine.stop!(true)
-      end
-    end
-
     DaFunk::EventHandler.new :schedule, minutes: 1440 do
       DaFunk::Engine.stop!(true)
     end
