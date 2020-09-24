cc79f1a7968b9788aaaf6aa26aa6209ee75e4201
William Roberto <william@cloudwalk.io>
2020-09-23 22:44:22 -0300

    Fixed display messages on application update proccess

--- lib/cloudwalk_update.rb
+++ lib/cloudwalk_update.rb
@@ -3,16 +3,10 @@ class CloudwalkUpdate
     return unless Device::Network.connected?
 
     Device::Display.clear
-    I18n.pt(:system_update_check)
-    Device::Display.print(I18n.t(:system_update_cancel), 2)
-    Device::Display.print("", 3)
-    key = Device::IO::KEY_TIMEOUT
-
-    10.times do |i|
-      Device::Display.print((10 - i).to_s, 5, 11)
-      key = getc(1000)
-      break if key != Device::IO::KEY_TIMEOUT
-    end
+    I18n.pt(:system_update_check, :line => 0)
+    I18n.pt(:system_update_cancel, :line => 3)
+    I18n.pt(:system_update_x_to_cancel, :line => 4)
+    key = count_down
 
     if File.exists?("./shared/application_update")
       File.delete("./shared/application_update")
